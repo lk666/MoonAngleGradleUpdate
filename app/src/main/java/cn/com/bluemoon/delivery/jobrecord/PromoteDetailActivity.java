160456695e51d5ce54b9a5925368425575613ead
liangjiangli <liangjiangli@bluemoon.com.cn>
2016-07-18 15:39:41 +0800

    多图片预览优化，推广点详情显示小区名称

--- app/src/main/java/cn/com/bluemoon/delivery/jobrecord/PromoteDetailActivity.java
+++ app/src/main/java/cn/com/bluemoon/delivery/jobrecord/PromoteDetailActivity.java
@@ -109,6 +109,7 @@ public class PromoteDetailActivity extends Activity {
                     Intent intent = new Intent(PromoteDetailActivity.this, ShowMultipleImageView.class);
                     Bundle bundle = new Bundle();
                     bundle.putSerializable("bitmaps", urls);
+                    intent.putExtra("position", position);
                     intent.putExtras(bundle);
                     startActivity(intent);
                 }
@@ -136,8 +137,8 @@ public class PromoteDetailActivity extends Activity {
                     txtOtherFee.setText(StringUtils.isNotBlank(info.getRemark()) ? info.getRemark() : getString(R.string.promote_none));
                     txtWifi.setText(info.getWifi()? getString(R.string.promote_has) : getString(R.string.promote_none));
                     txtNetwork.setText(info.getWiredNetwork()? getString(R.string.promote_has) : getString(R.string.promote_none));
-                    txtBpname.setText(String.format(getString(R.string.promote_append),info.getBpCode1(), info.getBpName1()));
                     txtBpname1.setText(String.format(getString(R.string.promote_append),info.getBpCode(), info.getBpName()));
+                    txtBpname.setText(String.format(getString(R.string.promote_append2),info.getBpCode1(), info.getBpName1(), info.getBpName2()));
                     images = info.getPicInfo();
                     if (images == null || images.size() == 0) {
                         layoutImage.setVisibility(View.GONE);
