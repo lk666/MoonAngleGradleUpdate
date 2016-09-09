96040b5fe83d8923ab847e766c05ad33edc9c3b3
万念 <wannian@bluemoon.com.cn>
2016-09-09 10:14:52 +0800

    单日绩效数据模型添加

---
+++ app/src/main/java/cn/com/bluemoon/delivery/sz/bean/taskManager/DailyPerformanceInfoBean.java
@@ -0,0 +1,156 @@
+package cn.com.bluemoon.delivery.sz.bean.taskManager;
+
+import java.io.Serializable;
+import java.util.List;
+
+import cn.com.bluemoon.delivery.sz.bean.MeetingerChooseBean.UserInfoDetailsBean;
+import cn.com.bluemoon.delivery.sz.bean.task.AsignJobBean;
+
+/**
+ * Created by Wan.N
+ * Date       2016/9/9
+ * Desc       单日绩效数据
+ */
+public class DailyPerformanceInfoBean implements Serializable {
+    /**
+     * asignJobs          工作任务列表<员工工作任务详情> array<object>
+     * createtime         创建时间 string
+     * day_score          日绩效积分 string
+     * day_valid_min      日实际有效工时（min） string
+     * model              模块 string
+     * reviewer           评价人 <个人信息结构> object
+     * uAvatar            头像Url string
+     * uID                用户ID string
+     * uName              用户姓名 string
+     * time_utilization   日时间有效利用率（百分比） string
+     * updatetime         更新时间 string
+     * user               <个人信息结构> object
+     * uAvatar            头像Url string
+     * uDept              部门 string
+     * uDeptID            部门ID string
+     * uID                用户ID string
+     * uName              用户姓名 string
+     * work_date          工作日期 string
+     * work_day_id        任务日计划ID
+     */
+    private List<AsignJobBean> asignJobs;
+    private String createtime;
+    private String day_score;
+    private String day_valid_min;
+    private String model;
+    private List<UserInfoDetailsBean> reviewer;
+    private String time_utilization;
+    private String updatetime;
+    private List<UserInfoDetailsBean> user;
+    private String work_date;
+    private String work_day_id;
+
+    public DailyPerformanceInfoBean() {
+    }
+
+    public List<AsignJobBean> getAsignJobs() {
+        return asignJobs;
+    }
+
+    public void setAsignJobs(List<AsignJobBean> asignJobs) {
+        this.asignJobs = asignJobs;
+    }
+
+    public String getCreatetime() {
+        return createtime;
+    }
+
+    public void setCreatetime(String createtime) {
+        this.createtime = createtime;
+    }
+
+    public String getDay_score() {
+        return day_score;
+    }
+
+    public void setDay_score(String day_score) {
+        this.day_score = day_score;
+    }
+
+    public String getDay_valid_min() {
+        return day_valid_min;
+    }
+
+    public void setDay_valid_min(String day_valid_min) {
+        this.day_valid_min = day_valid_min;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public List<UserInfoDetailsBean> getReviewer() {
+        return reviewer;
+    }
+
+    public void setReviewer(List<UserInfoDetailsBean> reviewer) {
+        this.reviewer = reviewer;
+    }
+
+    public String getTime_utilization() {
+        return time_utilization;
+    }
+
+    public void setTime_utilization(String time_utilization) {
+        this.time_utilization = time_utilization;
+    }
+
+    public String getUpdatetime() {
+        return updatetime;
+    }
+
+    public void setUpdatetime(String updatetime) {
+        this.updatetime = updatetime;
+    }
+
+    public List<UserInfoDetailsBean> getUser() {
+        return user;
+    }
+
+    public void setUser(List<UserInfoDetailsBean> user) {
+        this.user = user;
+    }
+
+    public String getWork_date() {
+        return work_date;
+    }
+
+    public void setWork_date(String work_date) {
+        this.work_date = work_date;
+    }
+
+    public String getWork_day_id() {
+        return work_day_id;
+    }
+
+    public void setWork_day_id(String work_day_id) {
+        this.work_day_id = work_day_id;
+    }
+
+    @Override
+    public String toString() {
+        return "DailyPerformanceInfoBean{" +
+                "asignJobs=" + asignJobs +
+                ", createtime='" + createtime + '\'' +
+                ", day_score='" + day_score + '\'' +
+                ", day_valid_min='" + day_valid_min + '\'' +
+                ", model='" + model + '\'' +
+                ", reviewer=" + reviewer +
+                ", time_utilization='" + time_utilization + '\'' +
+                ", updatetime='" + updatetime + '\'' +
+                ", user=" + user +
+                ", work_date='" + work_date + '\'' +
+                ", work_day_id='" + work_day_id + '\'' +
+                '}';
+    }
+}
+
