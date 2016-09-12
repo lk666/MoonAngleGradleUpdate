ac31750d6052e195d47fc928bd5c7907069e66e3
万念 <wannian@bluemoon.com.cn>
2016-09-12 09:15:41 +0800

    封装接口全

--- app/src/main/java/cn/com/bluemoon/delivery/sz/api/SzApi.java
+++ app/src/main/java/cn/com/bluemoon/delivery/sz/api/SzApi.java
@@ -15,6 +15,7 @@ import cn.com.bluemoon.delivery.AppContext;
 import cn.com.bluemoon.delivery.app.api.ApiHttpClient;
 import cn.com.bluemoon.delivery.common.ClientStateManager;
 import cn.com.bluemoon.delivery.sz.bean.taskManager.DailyPerformanceInfoBean;
+import cn.com.bluemoon.delivery.sz.bean.taskManager.RateDataInfoBean;
 import cn.com.bluemoon.delivery.utils.StringUtil;
 
 /**
@@ -94,9 +95,9 @@ public class SzApi {
      * token 用户token string
      * type 0:新增 1:修改 string
      */
-    public static void submitDayJobs(DailyPerformanceInfoBean submitData, String token, String type,
+    public static void submitDayJobs(DailyPerformanceInfoBean submitData, String type,
                                      AsyncHttpResponseHandler handler) {
-        if (submitData == null || StringUtil.isEmpty(token) || StringUtil.isEmpty(type)) {
+        if (submitData == null || StringUtil.isEmpty(type)) {
             return;
         }
         if (submitData.getAsignJobs() == null) {
@@ -108,7 +109,6 @@ public class SzApi {
         HashMap<String, Object> params = new HashMap<>();
         params.putAll(SzApiClientHelper.getParamUrl());
         params.put("submitData", submitData);
-        params.put("token", token);
         params.put("type", type);
 
         String url = HOST + "submitDayJobsApi";
@@ -118,10 +118,132 @@ public class SzApi {
 
     /**
      * desc  提交评价接口
-     *
-     * */
-    public static void submitDayJobsRating(){
+     * <p>
+     * rateData 评价数据[] array<object>
+     * work_day_id 任务日计划ID string
+     */
+    public static void submitDayJobsRating(List<RateDataInfoBean> rateData, String work_day_id,
+                                           AsyncHttpResponseHandler handler) {
+        if (rateData == null || StringUtil.isEmpty(work_day_id)) {
+            return;
+        }
 
+        HashMap<String, Object> params = new HashMap<>();
+        params.putAll(SzApiClientHelper.getParamUrl());
+        params.put("rateData", rateData);
+        params.put("work_day_id", work_day_id);
+
+        String url = HOST + "submitDayJobsRatingApi";
+        String jsonString = JSONObject.toJSONString(params);
+        ApiHttpClient.post(AppContext.getInstance(), url, jsonString, handler);
     }
 
+    /**
+     * desc 获取模糊查询员工联系人接口
+     * <p>
+     * queryStr 模糊查询关键字 string
+     */
+    public static void searchByKeyword(String queryStr, AsyncHttpResponseHandler handler) {
+        HashMap<String, Object> params = new HashMap<>();
+        params.putAll(SzApiClientHelper.getParamUrl());
+        params.put("queryStr", queryStr);
+
+        String url = HOST + "search";
+        String jsonString = JSONObject.toJSONString(params);
+        ApiHttpClient.post(AppContext.getInstance(), url, jsonString, handler);
+    }
+
+    /**
+     * desc 获得任务评价列表
+     * <p>
+     * currentPage 当前页(从1开始) string
+     * pageSize 每页数量 string
+     * type 0:待评价 1:已评价
+     */
+    public static void getRateJobsList(String currentPage, String pageSize, String type, AsyncHttpResponseHandler handler) {
+        HashMap<String, Object> params = new HashMap<>();
+        params.putAll(SzApiClientHelper.getParamUrl());
+        params.put("currentPage", currentPage);
+        params.put("pageSize", pageSize);
+        params.put("type", type);
+
+        String url = HOST + "getRateJobsListApi";
+        String jsonString = JSONObject.toJSONString(params);
+        ApiHttpClient.post(AppContext.getInstance(), url, jsonString, handler);
+    }
+
+    /**
+     * desc 获得单日工作任务列表&月度绩效积分组合接口
+     * <p>
+     * date 获取数据日期(GMT) string
+     * type 0:全部 1:任务列表 2:月度绩效 string
+     */
+    public static void getWorkDetails(String date, String type, AsyncHttpResponseHandler handler) {
+        HashMap<String, Object> params = new HashMap<>();
+        params.putAll(SzApiClientHelper.getParamUrl());
+        params.put("date", date);
+        params.put("type", type);
+
+        String url = HOST + "getWorkDetailsApi";
+        String jsonString = JSONObject.toJSONString(params);
+        ApiHttpClient.post(AppContext.getInstance(), url, jsonString, handler);
+    }
+
+    /**
+     * desc 获得建议详情接口
+     * <p>
+     * nID 通知ID string
+     */
+    public static void getSuggestDetail(String nID, AsyncHttpResponseHandler handler) {
+        HashMap<String, Object> params = new HashMap<>();
+        params.putAll(SzApiClientHelper.getParamUrl());
+        params.put("nID", nID);
+
+        String url = HOST + "getSuggestDetailApi";
+        String jsonString = JSONObject.toJSONString(params);
+        ApiHttpClient.post(AppContext.getInstance(), url, jsonString, handler);
+    }
+
+    /**
+     * desc 通过员工工号查询员工信息及直接上级信息
+     * <p>
+     * account 用户ID string
+     * appType [必填] app的类型 string [必填] app的类型: 培训平台:trainingSys 日程会议管理: scheduleSys 工作任务管理:moonAngel
+     * token 用户token string
+     */
+    public static void getUserAndSuperiorInfo(String account, AsyncHttpResponseHandler handler) {
+        HashMap<String, Object> params = new HashMap<>();
+        params.putAll(SzApiClientHelper.getParamUrl());
+        params.put("account", account);
+
+        String url = HOST + "getuserinfo";
+        String jsonString = JSONObject.toJSONString(params);
+        ApiHttpClient.post(AppContext.getInstance(), url, jsonString, handler);
+    }
+
+    /**
+     * desc 驳回接口
+     * <p>
+     * r_user_id 任务执行者编号（接收建议的人员编号） string
+     * rejectBody 驳回内容 string
+     * user_id 建议者ID string
+     * user_name 建议者姓名 string
+     * work_date 工作日期 string
+     * work_day_id 任务日计划ID string
+     */
+    public static void submitReject(String r_user_id, String rejectBody, String user_id, String user_name,
+                                              String work_date, String work_day_id, AsyncHttpResponseHandler handler) {
+        HashMap<String, Object> params = new HashMap<>();
+        params.putAll(SzApiClientHelper.getParamUrl());
+        params.put("r_user_id", r_user_id);
+        params.put("rejectBody", rejectBody);
+        params.put("user_id", user_id);
+        params.put("user_name", user_name);
+        params.put("work_date", work_date);
+        params.put("work_day_id", work_day_id);
+
+        String url = HOST + "submitRejectApi";
+        String jsonString = JSONObject.toJSONString(params);
+        ApiHttpClient.post(AppContext.getInstance(), url, jsonString, handler);
+    }
 }
