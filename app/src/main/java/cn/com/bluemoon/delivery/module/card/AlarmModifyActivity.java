e7699480eb1585e96f102b61436f92b5caaa9b3c
李超 <lichao@bluemoon.com.cn>
2016-09-13 11:03:13 +0800

    提交打卡相关代码

--- app/src/main/java/cn/com/bluemoon/delivery/module/card/AlarmModifyActivity.java
+++ app/src/main/java/cn/com/bluemoon/delivery/module/card/AlarmModifyActivity.java
@@ -1,53 +1,83 @@
 package cn.com.bluemoon.delivery.module.card;
 
-import android.view.Gravity;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.EditText;
-import android.widget.LinearLayout;
-import android.widget.NumberPicker;
-import android.widget.TimePicker;
+import android.util.Log;
 
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Date;
 
 import butterknife.Bind;
 import cn.com.bluemoon.delivery.R;
 import cn.com.bluemoon.delivery.app.api.model.ResultBase;
 import cn.com.bluemoon.delivery.module.base.BaseActivity;
+import cn.com.bluemoon.delivery.ui.selectordialog.OnSelectChangedListener;
+import cn.com.bluemoon.delivery.ui.selectordialog.SimpleWheelView;
 
 /**
  * Created by allenli on 2016/9/12.
  */
 public class AlarmModifyActivity extends BaseActivity {
 
-    @Bind(R.id.time_picker)
-    TimePicker timePicker;
+    @Bind(R.id.wheel_hour)
+    SimpleWheelView wheelHour;
+
+    @Bind(R.id.wheel_minute)
+    SimpleWheelView wheelMinute;
 
     @Override
     protected int getLayoutId() {
         return R.layout.activity_alarm_modify;
     }
 
+
+    private ArrayList<String> getHourList() {
+        ArrayList<String> list = new ArrayList<>();
+        for (int i = 0; i < 24; i++) {
+            if(i<10){
+                list.add("0"+i);
+            }else {
+                list.add(String.valueOf(i));
+            }
+        }
+        return list;
+    }
+
+    private ArrayList<String> getMinuteList() {
+        ArrayList<String> list = new ArrayList<>();
+        for (int i = 0; i < 60; i++) {
+            if(i<10){
+                list.add("0"+i);
+            }else {
+                list.add(String.valueOf(i));
+            }
+        }
+        return list;
+    }
+
+
     @Override
     public void initView() {
 
-        //设置成24小时，隐藏AM/PM picker
-        timePicker.setIs24HourView(true);
-        //修改TimePicker字体的大小
-        setNumberPickerTextSize(timePicker);
-        //修改TimePicker中NumberPicker的大小
-        resizeTimerPicker(timePicker);
+        Date date = new Date();
+
+        wheelHour.initData(getHourList(),2);
+        wheelMinute.initData(getHourList(),2);
 
 
-        timePicker.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener() {
+
+        wheelHour.setOnSelectListener(new OnSelectChangedListener() {
             @Override
-            public void onTimeChanged(TimePicker view, int hourOfDay, int minute) {
-                setNumberPickerTextSize(timePicker);
-                resizeTimerPicker(timePicker);
+            public void onEndSelected(int index, Object text) {
+                Log.e("onEndSelected", index + "," + text.toString());
             }
         });
 
+
+        wheelMinute.setOnSelectListener(new OnSelectChangedListener() {
+            @Override
+            public void onEndSelected(int index, Object text) {
+                Log.e("onEndSelected", index + "," + text.toString());
+            }
+        });
     }
 
     @Override
@@ -58,82 +88,6 @@ public class AlarmModifyActivity extends BaseActivity {
     @Override
     public void onSuccessResponse(int requestCode, String jsonString, ResultBase result) {
 
-    }
-
-    private List<NumberPicker> findNumberPicker(ViewGroup viewGroup)
-    {
-        List<NumberPicker> npList = new ArrayList<>();
-        View child = null;
-
-        if (null != viewGroup)
-        {
-            for (int i = 0; i < viewGroup.getChildCount(); i++)
-            {
-                child = viewGroup.getChildAt(i);
-                if (child instanceof NumberPicker)
-                {
-                    npList.add((NumberPicker)child);
-                }
-                else if (child instanceof LinearLayout)
-                {
-                    List<NumberPicker> result = findNumberPicker((ViewGroup)child);
-                    if (result.size() > 0)
-                    {
-                        return result;
-                    }
-                }
-            }
-        }
-
-        return npList;
-    }
-
-    private EditText findEditText(NumberPicker np)
-    {
-        if (null != np)
-        {
-            for (int i = 0; i < np.getChildCount(); i++)
-            {
-                View child = np.getChildAt(i);
-
-                if (child instanceof EditText)
-                {
-
-                    return (EditText)child;
-                }
-            }
-        }
-
-        return null;
-    }
-
-    private void setNumberPickerTextSize(ViewGroup viewGroup)
-    {
-        List<NumberPicker> npList = findNumberPicker(viewGroup);
-        if (null != npList)
-        {
-            for (NumberPicker np : npList)
-            {
-                EditText et = findEditText(np);
-              //  et.setFocusable(false);
-                et.setGravity(Gravity.CENTER);
-                et.setTextColor(getResources().getColor(R.color.text_black));
-                et.setTextSize(28);
-
-            }
-        }
-    }
-
-    private void resizeTimerPicker(TimePicker tp)
-    {
-        List<NumberPicker> npList = findNumberPicker(tp);
-
-        for (NumberPicker np : npList)
-        {
-            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(100, LinearLayout.LayoutParams.WRAP_CONTENT);
-            params.setMargins(10, 0, 10, 0);
-            np.setLayoutParams(params);
-        }
     }
 
 }
