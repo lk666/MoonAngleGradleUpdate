f97300122584a04c880ec8487e0c48063e03a717
chenchongsen <chenchongsen@bluemoon.com.cn>
2016-10-14 12:07:48 +0800

    修改API，改为测试环境；

--- app/src/main/java/cn/com/bluemoon/delivery/app/api/ReturningApi.java
+++ app/src/main/java/cn/com/bluemoon/delivery/app/api/ReturningApi.java
@@ -17,10 +17,10 @@ import cn.com.bluemoon.delivery.app.api.model.wash.manager.ImageInfo;
  * Created by lk on 2016/9/18.
  */
 public class ReturningApi extends DeliveryApi {
-    protected static void postRequest(Map<String, Object> params, String subUrl,
+    /*protected static void postRequest(Map<String, Object> params, String subUrl,
                                       AsyncHttpResponseHandler handler) {
         postRequest(params, subUrl, handler);
-    }
+    }*/
 
     /**
      * 1.1功能角标统计查询(ResultCornerNum)
