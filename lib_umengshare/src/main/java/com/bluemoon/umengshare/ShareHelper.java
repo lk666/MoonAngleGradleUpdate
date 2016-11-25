505083fdf46d8b9cd8bd27ed81e2dc473bfd422e
李超 <lichao@bluemoon.com.cn>
2016-11-25 15:29:19 +0800

    新增复制链接

--- lib_umengshare/src/main/java/com/bluemoon/umengshare/ShareHelper.java
+++ lib_umengshare/src/main/java/com/bluemoon/umengshare/ShareHelper.java
@@ -2,6 +2,8 @@ package com.bluemoon.umengshare;
 
 import android.app.Activity;
 import android.app.Application;
+import android.content.Context;
+import android.text.ClipboardManager;
 import android.widget.Toast;
 
 import com.umeng.socialize.PlatformConfig;
@@ -30,7 +32,7 @@ public class ShareHelper {
 	}
 
 	public static void share(final Activity activity, final ShareModel shareModel) {
-		share(activity,shareModel,null);
+		share(activity, shareModel, null);
 	}
 
 
@@ -42,7 +44,7 @@ public class ShareHelper {
 			public void onResult(SHARE_MEDIA platform) {
 				Toast.makeText(activity, shareMediaToChineseString(platform) + " 分享成功啦", Toast.LENGTH_SHORT).show();
 				if (null != callBack) {
-					callBack.shareSuccess(platform,shareMediaToString(platform), shareModel);
+					callBack.shareSuccess(platform, shareMediaToString(platform), shareModel);
 				}
 			}
 
@@ -50,7 +52,7 @@ public class ShareHelper {
 			public void onError(SHARE_MEDIA platform, Throwable t) {
 				Toast.makeText(activity, shareMediaToChineseString(platform) + " 分享失败啦", Toast.LENGTH_SHORT).show();
 				if (null != callBack) {
-					callBack.shareError(platform,shareMediaToString(platform), shareModel, t.getMessage());
+					callBack.shareError(platform, shareMediaToString(platform), shareModel, t.getMessage());
 				}
 			}
 
@@ -58,7 +60,7 @@ public class ShareHelper {
 			public void onCancel(SHARE_MEDIA platform) {
 				Toast.makeText(activity, shareMediaToChineseString(platform) + " 分享取消了", Toast.LENGTH_SHORT).show();
 				if (null != callBack) {
-					callBack.shareCancel(platform,shareMediaToString(platform), shareModel);
+					callBack.shareCancel(platform, shareMediaToString(platform), shareModel);
 				}
 			}
 		};
@@ -66,58 +68,76 @@ public class ShareHelper {
 		ShareBoardlistener boardListener = new ShareBoardlistener() {
 			@Override
 			public void onclick(SnsPlatform snsPlatform, SHARE_MEDIA share_media) {
+
 				if (null != callBack) {
-					callBack.boardClickCallBack(share_media,shareMediaToString(share_media), shareModel);
+					callBack.boardClickCallBack(share_media, shareMediaToString(share_media), shareModel);
 				}
 				String shareUrl = (shareModel.getuMTargetUrl().indexOf('?') > 0 ? shareModel.getuMTargetUrl() + "&platform=" : shareModel.getuMTargetUrl() + "?platform=")
-						+ ShareHelper.shareMediaToString(share_media);
-				new ShareAction(activity).setPlatform(share_media).setCallback(shareListener)
-						.withTitle(shareModel.getuMTitle())
-						.withText(shareModel.getuMText())
-						.withTargetUrl(shareUrl)
-						.withMedia(shareModel.getuMImage())
-						.share();
+						+ shareMediaToString(share_media);
 
+				if (share_media == null) {
+					if (snsPlatform.mKeyword.equals("share_button_custom_link")) {
+						ClipboardManager cmb = (ClipboardManager) activity.getSystemService(Context.CLIPBOARD_SERVICE);
+						cmb.setText(shareUrl);
+						Toast.makeText(activity, "复制链接成功", Toast.LENGTH_LONG).show();
+					}
+
+				} else {
+
+
+					new ShareAction(activity).setPlatform(share_media).setCallback(shareListener)
+							.withTitle(shareModel.getuMTitle())
+							.withText(shareModel.getuMText())
+							.withTargetUrl(shareUrl)
+							.withMedia(shareModel.getuMImage())
+							.share();
+				}
 			}
 		};
 		new ShareAction(activity)
 				.setDisplayList(SHARE_MEDIA.SINA, SHARE_MEDIA.QQ, SHARE_MEDIA.QZONE, SHARE_MEDIA.WEIXIN, SHARE_MEDIA.WEIXIN_CIRCLE, SHARE_MEDIA.WEIXIN_FAVORITE)
+				.addButton("share_button_custom_link", "share_button_custom_link", "link", "link")
 				.setShareboardclickCallback(boardListener).open();
 
 	}
 
 	private static String shareMediaToString(SHARE_MEDIA share_media) {
-		if (share_media.equals(SHARE_MEDIA.QQ)) {
-			return "QQ";
-		} else if (share_media.equals(SHARE_MEDIA.QZONE)) {
-			return "qzone";
-		} else if (share_media.equals(SHARE_MEDIA.SINA)) {
-			return "sina";
-		} else if (share_media.equals(SHARE_MEDIA.WEIXIN)) {
-			return "wechatSession";
-		} else if (share_media.equals(SHARE_MEDIA.WEIXIN_CIRCLE)) {
-			return "wechatTimeLine";
-		} else if (share_media.equals(SHARE_MEDIA.WEIXIN_FAVORITE)) {
-			return "wechatFavorite";
+		if (null != share_media) {
+			if (share_media.equals(SHARE_MEDIA.QQ)) {
+				return "QQ";
+			} else if (share_media.equals(SHARE_MEDIA.QZONE)) {
+				return "qzone";
+			} else if (share_media.equals(SHARE_MEDIA.SINA)) {
+				return "sina";
+			} else if (share_media.equals(SHARE_MEDIA.WEIXIN)) {
+				return "wechatSession";
+			} else if (share_media.equals(SHARE_MEDIA.WEIXIN_CIRCLE)) {
+				return "wechatTimeLine";
+			} else if (share_media.equals(SHARE_MEDIA.WEIXIN_FAVORITE)) {
+				return "wechatFavorite";
+			}
 		}
-		return "";
+		return "link";
 	}
 
 	private static String shareMediaToChineseString(SHARE_MEDIA share_media) {
-		if (share_media.equals(SHARE_MEDIA.QQ)) {
-			return "QQ";
-		} else if (share_media.equals(SHARE_MEDIA.QZONE)) {
-			return "QQ空间";
-		} else if (share_media.equals(SHARE_MEDIA.SINA)) {
-			return "新浪";
-		} else if (share_media.equals(SHARE_MEDIA.WEIXIN)) {
-			return "微信";
-		} else if (share_media.equals(SHARE_MEDIA.WEIXIN_CIRCLE)) {
-			return "微信朋友圈";
-		} else if (share_media.equals(SHARE_MEDIA.WEIXIN_FAVORITE)) {
-			return "微信收藏";
+		if (null != share_media) {
+			if (share_media.equals(SHARE_MEDIA.QQ)) {
+				return "QQ";
+			} else if (share_media.equals(SHARE_MEDIA.QZONE)) {
+				return "QQ空间";
+			} else if (share_media.equals(SHARE_MEDIA.SINA)) {
+				return "新浪";
+			} else if (share_media.equals(SHARE_MEDIA.WEIXIN)) {
+				return "微信";
+			} else if (share_media.equals(SHARE_MEDIA.WEIXIN_CIRCLE)) {
+				return "微信朋友圈";
+			} else if (share_media.equals(SHARE_MEDIA.WEIXIN_FAVORITE)) {
+				return "微信收藏";
+			}
 		}
-		return "";
+		return "复制链接";
 	}
+
 
 }
