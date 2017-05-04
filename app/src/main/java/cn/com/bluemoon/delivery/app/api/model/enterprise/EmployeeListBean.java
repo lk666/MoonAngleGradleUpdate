7467cd6f01f0ad7b5aa3d62d46e9951960cff188
luokai <luokai@bluemoon.com.cn>
2017-05-04 11:46:40 +0800

    8.02手动搜索列表展示接口及其bean

---
+++ app/src/main/java/cn/com/bluemoon/delivery/app/api/model/enterprise/EmployeeListBean.java
@@ -0,0 +1,19 @@
+package cn.com.bluemoon.delivery.app.api.model.enterprise;
+
+import java.io.Serializable;
+
+public class EmployeeListBean implements Serializable {
+
+        /** 机构编码 */
+        public String branchCode;
+        /** 机构名称 */
+        public String branchName;
+        /** 账户id */
+        public String employeeCode;
+        /** 分机号 */
+        public String employeeExtension;
+        /** 员工姓名 */
+        public String employeeName;
+        /** 员工手机号 */
+        public String employeePhone;
+    }
---
+++ app/src/main/java/cn/com/bluemoon/delivery/app/api/model/enterprise/ResultGetWashEnterpriseQuery.java
@@ -0,0 +1,19 @@
+package cn.com.bluemoon.delivery.app.api.model.enterprise;
+
+import java.util.List;
+
+import cn.com.bluemoon.delivery.app.api.model.ResultBase;
+
+/**
+ * 8.02手动搜索列表展示
+ */
+public class ResultGetWashEnterpriseQuery extends ResultBase {
+    /**
+     * 员工信息
+     */
+    public List<EmployeeListBean> employeeList;
+    /**
+     * 订单信息
+     */
+    public List<EnterpriseOrderListBean> enterpriseOrderList;
+}
