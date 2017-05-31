65c2b43d5460a806056eeb954c8bf75b5a85074f
BM1001213C\Administrator <tangqiwei@bluemoon.com.cn>
2017-05-31 11:51:08 +0800

    完成bm_segment

--- app/src/main/java/cn/com/bluemoon/delivery/ui/common/TestUiAcitivty.java
+++ app/src/main/java/cn/com/bluemoon/delivery/ui/common/TestUiAcitivty.java
@@ -3,6 +3,9 @@ package cn.com.bluemoon.delivery.ui.common;
 import android.app.Activity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import cn.com.bluemoon.delivery.R;
 
 /**
@@ -10,12 +13,22 @@ import cn.com.bluemoon.delivery.R;
  */
 
 public class TestUiAcitivty extends Activity {
-    BmMarkView markView;
+    BmSegmentView segmentView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test_ui);
-        markView= (BmMarkView) findViewById(R.id.mark_view);
-        markView.setMarkViewWidthAndText(555);
+        segmentView= (BmSegmentView) findViewById(R.id.segment_view);
+
+        List<String> textList=new ArrayList<>();
+        textList.add("洗衣液");
+        textList.add("洗洁精");
+        textList.add("洁厕剂");
+        segmentView.setTextList(textList);
+        List<Integer> numberList=new ArrayList<>();
+        numberList.add(123);
+        numberList.add(0);
+        numberList.add(34);
+        segmentView.setNumberList(numberList);
     }
 }
