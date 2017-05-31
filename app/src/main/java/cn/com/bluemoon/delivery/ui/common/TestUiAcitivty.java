6729b0a07805e2ba79f8beb4a9b83a1b1e51e303
BM1001213C\Administrator <tangqiwei@bluemoon.com.cn>
2017-05-31 16:46:14 +0800

    bm_rank_star1完成

--- app/src/main/java/cn/com/bluemoon/delivery/ui/common/TestUiAcitivty.java
+++ app/src/main/java/cn/com/bluemoon/delivery/ui/common/TestUiAcitivty.java
@@ -13,22 +13,11 @@ import cn.com.bluemoon.delivery.R;
  */
 
 public class TestUiAcitivty extends Activity {
-    BmSegmentView segmentView;
+    BmRankStar1 bmRankStar1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test_ui);
-        segmentView= (BmSegmentView) findViewById(R.id.segment_view);
-
-        List<String> textList=new ArrayList<>();
-        textList.add("洗衣液");
-        textList.add("洗洁精");
-        textList.add("洁厕剂");
-        segmentView.setTextList(textList);
-        List<Integer> numberList=new ArrayList<>();
-        numberList.add(123);
-        numberList.add(0);
-        numberList.add(34);
-        segmentView.setNumberList(numberList);
+        bmRankStar1= (BmRankStar1) findViewById(R.id.bmrankstar_view);
     }
 }
