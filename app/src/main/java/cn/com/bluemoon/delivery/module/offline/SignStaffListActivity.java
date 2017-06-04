b4409197a43e7339e95a3772d88f26443e916823
BM1001213C\Administrator <tangqiwei@bluemoon.com.cn>
2017-06-04 16:41:02 +0800

    改进学员评价列表ADAPTER

--- app/src/main/java/cn/com/bluemoon/delivery/module/offline/SignStaffListActivity.java
+++ app/src/main/java/cn/com/bluemoon/delivery/module/offline/SignStaffListActivity.java
@@ -3,22 +3,15 @@ package cn.com.bluemoon.delivery.module.offline;
 import android.content.Context;
 import android.content.Intent;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.LinearLayout;
-import android.widget.TextView;
 
 import java.util.List;
-
-import cn.com.bluemoon.delivery.R;
 import cn.com.bluemoon.delivery.app.api.OffLineApi;
 import cn.com.bluemoon.delivery.app.api.model.ResultBase;
 import cn.com.bluemoon.delivery.app.api.model.offline.ResultSignStaffList;
 import cn.com.bluemoon.delivery.module.base.BaseListAdapter;
 import cn.com.bluemoon.delivery.module.base.BasePullToRefreshListViewActivity;
-import cn.com.bluemoon.delivery.module.base.OnListItemClickListener;
+import cn.com.bluemoon.delivery.module.offline.adapter.SignStaffAdapter;
 import cn.com.bluemoon.delivery.ui.ShowEvaluateDetailPopView;
-import cn.com.bluemoon.delivery.utils.Constants;
-import cn.com.bluemoon.delivery.utils.DateUtil;
 import cn.com.bluemoon.lib.pulltorefresh.PullToRefreshBase;
 
 /**
@@ -87,40 +80,4 @@ public class SignStaffListActivity extends BasePullToRefreshListViewActivity imp
 
     }
 
-    class SignStaffAdapter extends BaseListAdapter<ResultSignStaffList.Data.Students>{
-
-        public SignStaffAdapter(Context context, OnListItemClickListener listener) {
-            super(context, listener);
-        }
-
-        @Override
-        protected int getLayoutId() {
-            return R.layout.item_sign_staff;
-        }
-
-        @Override
-        protected void setView(int position, View convertView, ViewGroup parent, boolean isNew) {
-            TextView nameView=getViewById(R.id.txt_name);
-            TextView codeView=getViewById(R.id.txt_code);
-            TextView signTimeView=getViewById(R.id.txt_sign_time);
-            TextView gradeView=getViewById(R.id.txt_grade);
-            TextView evaluateView=getViewById(R.id.txt_evaluate);
-            LinearLayout layoutDetail=getViewById(R.id.llayout_detail);
-
-            ResultSignStaffList.Data.Students student= (ResultSignStaffList.Data.Students) getItem(position);
-            nameView.setText(student.studentName);
-            codeView.setText(student.studentCode);
-            signTimeView.setText(DateUtil.getTimeToYMDHM(student.assignTime));
-
-            if(student.score>0){
-                layoutDetail.setVisibility(View.VISIBLE);
-                gradeView.setText(String.valueOf(student.score));
-                evaluateView.setText(getString(R.string.offline_type_evaluate)+student.comment);
-            }else{
-                layoutDetail.setVisibility(View.GONE);
-            }
-
-            setClickEvent(isNew,position,convertView);
-        }
-    }
 }
