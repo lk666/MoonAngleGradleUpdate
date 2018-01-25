af9f3a78e159c113f45cec5cab027f34a80d09fb
chenchongsen <chenchongsen@bluemoon.com.cn>
2018-01-25 15:47:15 +0800

    修复文件夹创建问题；

--- app/src/main/java/cn/com/bluemoon/delivery/MainActivity.java
+++ app/src/main/java/cn/com/bluemoon/delivery/MainActivity.java
@@ -46,6 +46,7 @@ import cn.com.bluemoon.delivery.module.track.TrackManager;
 import cn.com.bluemoon.delivery.ui.AlwaysMarqueeTextView;
 import cn.com.bluemoon.delivery.utils.Constants;
 import cn.com.bluemoon.delivery.utils.DialogUtil;
+import cn.com.bluemoon.delivery.utils.FileUtil;
 import cn.com.bluemoon.delivery.utils.PublicUtil;
 import cn.com.bluemoon.delivery.utils.StringUtil;
 import cn.com.bluemoon.delivery.utils.ViewUtil;
@@ -96,6 +97,7 @@ public class MainActivity extends BaseSlidingActivity implements View.OnClickLis
 
     @Override
     public void initView() {
+        FileUtil.init();
         //初始化侧滑栏
         initMenu();
         //兼容沉浸式
