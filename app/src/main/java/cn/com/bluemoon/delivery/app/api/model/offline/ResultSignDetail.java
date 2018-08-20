de01c377419f1b6e109418dc5523a2a8c5e561ee
chenchongsen <chenchongsen@bluemoon.com.cn>
2018-08-20 13:24:37 +0800

    学员多课程签到接口联调；

--- app/src/main/java/cn/com/bluemoon/delivery/app/api/model/offline/ResultSignDetail.java
+++ app/src/main/java/cn/com/bluemoon/delivery/app/api/model/offline/ResultSignDetail.java
@@ -9,39 +9,40 @@ import cn.com.bluemoon.delivery.app.api.model.ResultBase;
  * Created by bm on 2017/5/27.
  */
 
-public class ResultSignDetail extends ResultBase {
+public class ResultSignDetail extends ResultBase  {
 
     public SignDetailData data;
 
     public static class SignDetailData implements Serializable{
 
         public long date;
-        public PlanInfo planInfo;
         public String room;
+        public PlanInfo planInfo;
         public String type;
         public List<Course> courses;
 
-        public static class PlanInfo implements Serializable{
+        public static class PlanInfo implements Serializable {
 
-            public int endTime;
+            public long startTime;
+            public long endTime;
             public long nowDate;
             public String planCode;
-            public int startTime;
             public String topic;
         }
 
-        public static class Course implements Serializable{
+        public static class Course implements Serializable {
 
-            public String courseCode;
-            public String courseName;
+            public long startTime;
             public long endTime;
+            public String courseName;
+            public String teacherName;
+            public String courseCode;
+            public String planCode;
+            public int isDisable;
             public int isSign;
             public String message;
-            public String planCode;
             public String room;
-            public long startTime;
             public String status;
-            public String teacherName;
         }
     }
 }
